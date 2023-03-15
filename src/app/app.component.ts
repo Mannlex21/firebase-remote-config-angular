358e348f311bf980989c34b9e5625e8dc96bfd2c
Mannlex Murillo <mannlexx21@gmail.com>
2023-03-14 22:06:52 -0700

    add: cdn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,23 +26,22 @@ export class AppComponent implements AfterViewInit, OnInit {
   };
   constructor(private remoteConfig: RemoteConfig) {}
   ngOnInit(): void {
-    this.loading = true;
-    this.remoteConfig.settings.minimumFetchIntervalMillis = 1000; // configuración opcional
-    fetchAndActivate(this.remoteConfig).then((data) => {
-      // realizar acciones después de recuperar y activar la configuración remota
-      const v = getValue(this.remoteConfig, 'theme').asString();
-      const json = JSON.parse(v);
-      var style = document.createElement('style');
-      let string = '';
-      for (const key in json['theme']) {
-        string += `${key}: ${json['theme'][key].value};`;
-      }
-
-      style.textContent = `body { ${string} }`;
-      console.log(style.textContent);
-      document.head.appendChild(style);
-      this.loading = false;
-    });
+    // this.loading = true;
+    // this.remoteConfig.settings.minimumFetchIntervalMillis = 1000; // configuración opcional
+    // fetchAndActivate(this.remoteConfig).then((data) => {
+    //   // realizar acciones después de recuperar y activar la configuración remota
+    //   const v = getValue(this.remoteConfig, 'theme').asString();
+    //   const json = JSON.parse(v);
+    //   var style = document.createElement('style');
+    //   let string = '';
+    //   for (const key in json['theme']) {
+    //     string += `${key}: ${json['theme'][key].value};`;
+    //   }
+    //   style.textContent = `body { ${string} }`;
+    //   console.log(style.textContent);
+    //   document.head.appendChild(style);
+    //   this.loading = false;
+    // });
   }
   toggleMenu($event: Boolean) {
     this.showMenu = !this.showMenu;
